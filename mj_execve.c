4dad7431b4aeebcea5b8f2efa207b169e2f505da
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-06-26 04:13:37 +0400

    changes in mj_execve.c

--- mj_execve.c
+++ mj_execve.c
@@ -11,24 +11,11 @@ void mj_execve(char **args)
 {
 	pid_t mj_pid;
 	int status;
-	/*int mj_pipe[2];*/
-
-	/*if (pipe(mj_pipe) == -1)
-	{
-		fprintf(stderr, "Error: Failed to pipe\n");
-		exit(EXIT_FAILURE);
-	}*/
-		
+			
 	mj_pid = fork();
 
 	if (mj_pid == 0)
 	{
-		/*close(mj_pipe[1]);
-
-		dup2(mj_pipe[0], STDIN_FILENO);
-
-		close(mj_pipe[0]);*/
-
 		execvp(args[0], args);
 		fprintf(stderr, "%s: 1: %s: not found\n", args[0], args[0]);
 		exit(EXIT_FAILURE);
@@ -40,12 +27,6 @@ void mj_execve(char **args)
 	}
 	else
 	{
-		/*close(mj_pipe[0]);
-
-		dprintf(mj_pipe[1], "%s\n", args[0]);
-
-		close(mj_pipe[1]);*/
-
 		do {
 			waitpid(mj_pid, &status, WUNTRACED);
 
