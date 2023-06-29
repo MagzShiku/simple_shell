898b5a426b07d8cc4275ebbadfc9d84d09bbe062
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-06-29 13:01:40 +0400

    new try

---
+++ 0_main.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "source.h"
+#include "parser.h"
+#include "executor.h"
+#include "shell.h"
+
+struct source_s src;
+    
+char* read_cmd(void);
+int parse_and_execute(struct source_s* src);
+    struct token_s* tok;
+     struct node_s* cmd;
+
+int main(int argc, char** argv)
+{
+     char *cmd;
+	
+	(void)argc;
+    (void)argv;
+
+   /* struct source_s src;*/
+
+    do
+    {
+        print_prompt1();
+        cmd = read_cmd();
+        if (!cmd)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        if (cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+        {
+            free(cmd);
+            continue;
+        }
+        if (strcmp(cmd, "exit\n") == 0)
+        {
+            free(cmd);
+            break;
+        }
+
+        src.buffer = cmd;
+        src.bufsize = strlen(cmd);
+        src.curpos = INIT_SRC_POS;
+        parse_and_execute(&src);
+        free(cmd);
+    } while (1);
+
+    exit(EXIT_SUCCESS);
+}
+
+char* read_cmd(void)
+{
+    char buf[1024];
+    char* ptr = NULL;
+    char ptrlen = 0;
+    char* ptr2;
+    int buflen;
+
+    while (fgets(buf, 1024, stdin))
+    {
+        buflen = strlen(buf);
+
+        if (!ptr)
+        {
+            ptr = malloc(buflen + 1);
+        }
+        else
+        {
+            ptr2 = realloc(ptr, ptrlen + buflen + 1);
+
+            if (ptr2)
+            {
+                ptr = ptr2;
+            }
+            else
+            {
+                free(ptr);
+                ptr = NULL;
+            }
+        }
+
+        if (!ptr)
+        {
+            fprintf(stderr, "error: failed to alloc buffer: %s\n", strerror(errno));
+            return NULL;
+        }
+
+        strcpy(ptr + ptrlen, buf);
+
+        if (buf[buflen - 1] == '\n')
+        {
+            if (buflen == 1 || buf[buflen - 2] != '\\')
+            {
+                return ptr;
+            }
+
+            ptr[ptrlen + buflen - 2] = '\0';
+            buflen -= 2;
+            print_prompt2();
+        }
+
+        ptrlen += buflen;
+    }
+
+    return ptr;
+}
+
+int parse_and_execute(struct source_s* src)
+{
+    skip_white_spaces(src);
+
+    tok = tokenize(src);
+     /*struct node_s* cmd;*/
+
+    if (tok == &eof_token)
+    {
+        return 0;
+    }
+
+    while (tok && tok != &eof_token)
+    {
+        cmd = parse_simple_command(tok);
+        if (!cmd)
+        {
+            break;
+        }
+        do_simple_command(cmd);
+        free_node_tree(cmd);
+        tok = tokenize(src);
+    }
+    return 1;
+}
+
